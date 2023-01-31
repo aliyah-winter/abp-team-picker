d5225be43ae0f7536496feb4466299084b1c14ac
Aliyah Winter <aliyahgwinter@gmail.com>
2023-02-01 12:27:13 +1300

    Add dividing lines to results cards

--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -5,56 +5,56 @@ function Results() {
     <>
       <section className="md:flex md:gap-4 md:justify-items-center">
         <ul className="m-9 p-4 shadow-xl shadow-blue-500 rounded-xl bg-blue-100 text-center">
-          <li>WEEK 1</li>
-          <br></br>
+          <h1 className="text-xl font-bold">WEEK 1</h1>
+          <hr className="bg-black h-0.5 my-3" />
           <li>BIBLICAL</li>
           <li>vs</li>
           <li>Team 4</li>
-          <br></br>
+          <hr className="bg-black h-0.5 my-3" />
           <li>HAMbassadors</li>
           <li>vs</li>
           <li>1 OUTZ</li>
         </ul>
         <ul className="m-9 p-4 shadow-xl shadow-blue-500 rounded-xl bg-blue-100 text-center">
-          <li>WEEK 2</li>
-          <br></br>
+          <h1>WEEK 2</h1>
+          <hr className="bg-black h-0.5 my-3" />
           <li>1 OUTZ</li>
           <li>vs</li>
           <li>BIBLICAL</li>
-          <br></br>
+          <hr className="bg-black h-0.5 my-3" />
           <li>TEAM 4</li>
           <li>vs</li>
           <li>TEAM 5</li>
         </ul>
         <ul className="m-9 p-4 shadow-xl shadow-blue-500 rounded-xl bg-blue-100 text-center">
-          <li>WEEK 3</li>
-          <br></br>
+          <h1>WEEK 3</h1>
+          <hr className="bg-black h-0.5 my-3" />
           <li>TEAM 5</li>
           <li>vs</li>
           <li>1 OUTZ</li>
-          <br></br>
+          <hr className="bg-black h-0.5 my-3" />
           <li>BIBLICAL</li>
           <li>vs</li>
           <li>HAMbassadors</li>
         </ul>
         <ul className="m-9 p-4 shadow-xl shadow-blue-500 rounded-xl bg-blue-100 text-center">
-          <li>WEEK 4</li>
-          <br></br>
+          <h1>WEEK 4</h1>
+          <hr className="bg-black h-0.5 my-3" />
           <li>HAMbassadors</li>
           <li>vs</li>
           <li>Team 5</li>
-          <br></br>
+          <hr className="bg-black h-0.5 my-3" />
           <li>1 OUTZ</li>
           <li>vs</li>
           <li>Team 4</li>
         </ul>
         <ul className="m-9 p-4 shadow-xl shadow-blue-500 rounded-xl bg-blue-100 text-center">
-          <li>WEEK 5</li>
-          <br></br>
+          <h1>WEEK 5</h1>
+          <hr className="bg-black h-0.5 my-3" />
           <li>Team 4</li>
           <li>vs</li>
           <li>HAMbassadors</li>
-          <br></br>
+          <hr className="bg-black h-0.5 my-3" />
           <li>TEAM 5</li>
           <li>vs</li>
           <li>BIBLICAL</li>
