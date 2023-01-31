7a46992cc22629be69759f2bf9fbc2437b6dfeac
Aliyah Winter <aliyahgwinter@gmail.com>
2023-01-31 16:36:38 +1300

    Add breakpoints to stats and results

--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -3,51 +3,53 @@ import React from 'react'
 function Results() {
   return (
     <>
-      <ul className="m-9 p-4 shadow-xl shadow-blue-500 rounded-xl bg-blue-100 text-center">
-        <li>Week 1</li>
-        <br></br>
-        <li>Team 1</li>
-        <li>Team 4</li>
-        <br></br>
-        <li>Team 2</li>
-        <li>Team 3</li>
-      </ul>
-      <ul className="m-9 p-4 shadow-xl shadow-blue-500 rounded-xl bg-blue-100 text-center">
-        <li>Week 1</li>
-        <br></br>
-        <li>Team 1</li>
-        <li>Team 4</li>
-        <br></br>
-        <li>Team 2</li>
-        <li>Team 3</li>
-      </ul>
-      <ul className="m-9 p-4 shadow-xl shadow-blue-500 rounded-xl bg-blue-100 text-center">
-        <li>Week 1</li>
-        <br></br>
-        <li>Team 1</li>
-        <li>Team 4</li>
-        <br></br>
-        <li>Team 2</li>
-        <li>Team 3</li>
-      </ul>
-      <ul className="m-9 p-4 shadow-xl shadow-blue-500 rounded-xl bg-blue-100 text-center">
-        <li>Week 1</li>
-        <br></br>
-        <li>Team 1</li>
-        <li>Team 4</li>
-        <br></br>
-        <li>Team 2</li>
-        <li>Team 3</li>
-      </ul>
-      <ul className="m-9 p-4 shadow-xl shadow-blue-500 rounded-xl bg-blue-100 text-center">
-        <li>Week 1</li>
-        <br></br>
-        <li>Team 1</li>
-        <li>Team 4</li>
-        <br></br>
-        <li>Team 2</li>
-        <li>Team 3</li>
-      </ul>
+      <section className="md:flex md:gap-4 md:justify-items-center">
+        <ul className="m-9 p-4 shadow-xl shadow-blue-500 rounded-xl bg-blue-100 text-center">
+          <li>Week 1</li>
+          <br></br>
+          <li>Team 1</li>
+          <li>Team 4</li>
+          <br></br>
+          <li>Team 2</li>
+          <li>Team 3</li>
+        </ul>
+        <ul className="m-9 p-4 shadow-xl shadow-blue-500 rounded-xl bg-blue-100 text-center">
+          <li>Week 1</li>
+          <br></br>
+          <li>Team 1</li>
+          <li>Team 4</li>
+          <br></br>
+          <li>Team 2</li>
+          <li>Team 3</li>
+        </ul>
+        <ul className="m-9 p-4 shadow-xl shadow-blue-500 rounded-xl bg-blue-100 text-center">
+          <li>Week 1</li>
+          <br></br>
+          <li>Team 1</li>
+          <li>Team 4</li>
+          <br></br>
+          <li>Team 2</li>
+          <li>Team 3</li>
+        </ul>
+        <ul className="m-9 p-4 shadow-xl shadow-blue-500 rounded-xl bg-blue-100 text-center">
+          <li>Week 1</li>
+          <br></br>
+          <li>Team 1</li>
+          <li>Team 4</li>
+          <br></br>
+          <li>Team 2</li>
+          <li>Team 3</li>
+        </ul>
+        <ul className="m-9 p-4 shadow-xl shadow-blue-500 rounded-xl bg-blue-100 text-center">
+          <li>Week 1</li>
+          <br></br>
+          <li>Team 1</li>
+          <li>Team 4</li>
+          <br></br>
+          <li>Team 2</li>
+          <li>Team 3</li>
+        </ul>
+      </section>
     </>
   )
 }
--- client/components/Stats.jsx
+++ client/components/Stats.jsx
@@ -3,56 +3,58 @@ import React from 'react'
 function Stats() {
   return (
     <>
-      <ul className="m-9 p-4 shadow-xl shadow-green-500 rounded-xl bg-green-100 text-center">
-        <li>Team 1</li>
-        <br></br>
-        <li>WINS</li>
-        <li>LOSSES</li>
-        <br></br>
-        <li>Player 1</li>
-        <li>Player 2</li>
-        <li>Player 3</li>
-      </ul>
-      <ul className="m-9 p-4 shadow-xl shadow-green-500 rounded-xl bg-green-100 text-center">
-        <li>Team 2</li>
-        <br></br>
-        <li>WINS</li>
-        <li>LOSSES</li>
-        <br></br>
-        <li>Player 1</li>
-        <li>Player 2</li>
-        <li>Player 3</li>
-      </ul>
-      <ul className="m-9 p-4 shadow-xl shadow-green-500 rounded-xl bg-green-100 text-center">
-        <li>Team 3</li>
-        <br></br>
-        <li>WINS</li>
-        <li>LOSSES</li>
-        <br></br>
-        <li>Player 1</li>
-        <li>Player 2</li>
-        <li>Player 3</li>
-      </ul>
-      <ul className="m-9 p-4 shadow-xl shadow-green-500 rounded-xl bg-green-100 text-center">
-        <li>Team 4</li>
-        <br></br>
-        <li>WINS</li>
-        <li>LOSSES</li>
-        <br></br>
-        <li>Player 1</li>
-        <li>Player 2</li>
-        <li>Player 3</li>
-      </ul>
-      <ul className="m-9 p-4 shadow-xl shadow-green-500 rounded-xl bg-green-100 text-center">
-        <li>Team 5</li>
-        <br></br>
-        <li>WINS</li>
-        <li>LOSSES</li>
-        <br></br>
-        <li>Player 1</li>
-        <li>Player 2</li>
-        <li>Player 3</li>
-      </ul>
+      <section className="md:flex md:gap-4 md:justify-items-center">
+        <ul className="m-9 p-4 shadow-xl shadow-green-500 rounded-xl bg-green-100 text-center">
+          <li>Team 1</li>
+          <br></br>
+          <li>WINS</li>
+          <li>LOSSES</li>
+          <br></br>
+          <li>Player 1</li>
+          <li>Player 2</li>
+          <li>Player 3</li>
+        </ul>
+        <ul className="m-9 p-4 shadow-xl shadow-green-500 rounded-xl bg-green-100 text-center">
+          <li>Team 2</li>
+          <br></br>
+          <li>WINS</li>
+          <li>LOSSES</li>
+          <br></br>
+          <li>Player 1</li>
+          <li>Player 2</li>
+          <li>Player 3</li>
+        </ul>
+        <ul className="m-9 p-4 shadow-xl shadow-green-500 rounded-xl bg-green-100 text-center">
+          <li>Team 3</li>
+          <br></br>
+          <li>WINS</li>
+          <li>LOSSES</li>
+          <br></br>
+          <li>Player 1</li>
+          <li>Player 2</li>
+          <li>Player 3</li>
+        </ul>
+        <ul className="m-9 p-4 shadow-xl shadow-green-500 rounded-xl bg-green-100 text-center">
+          <li>Team 4</li>
+          <br></br>
+          <li>WINS</li>
+          <li>LOSSES</li>
+          <br></br>
+          <li>Player 1</li>
+          <li>Player 2</li>
+          <li>Player 3</li>
+        </ul>
+        <ul className="m-9 p-4 shadow-xl shadow-green-500 rounded-xl bg-green-100 text-center">
+          <li>Team 5</li>
+          <br></br>
+          <li>WINS</li>
+          <li>LOSSES</li>
+          <br></br>
+          <li>Player 1</li>
+          <li>Player 2</li>
+          <li>Player 3</li>
+        </ul>
+      </section>
     </>
   )
 }
