b3b86fe91139146c494c10931e07a4089760ba4e
Aliyah Winter <aliyahgwinter@gmail.com>
2023-02-01 17:30:22 +1300

    revert to no index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-const server = require('./server')
+// const server = require('./server')
 
-const port = `0.0.0.0:$PORT`
+// const port = `0.0.0.0:$PORT`
 
-server.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log('Server listening...')
-})
+// server.listen(port, () => {
+//   // eslint-disable-next-line no-console
+//   console.log('Server listening...')
+// })
