f3e38eaa72dc104fbb0e88cb61ec628c37e9204d
Aliyah Winter <aliyahgwinter@gmail.com>
2023-02-01 17:15:31 +1300

    Follow shymala example

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react({
-      fastRefresh: process.env.NODE_ENV !== 'test',
-    }),
-  ],
+  plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
+    watch: {
+      usePolling: true,
     },
+    host: true,
+    strictPort: true,
   },
+  // [
+  //   react({
+  //     fastRefresh: process.env.NODE_ENV !== 'test',
+  //   }),
+  // ],
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:3001',
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
 })
