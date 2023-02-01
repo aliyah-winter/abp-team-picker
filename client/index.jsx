3b84f407a00b4ee383ee2d06808df03d671432f6
Aliyah Winter <aliyahgwinter@gmail.com>
2023-02-01 16:03:03 +1300

    Clean up files for deploy

--- client/index.jsx
+++ client/index.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { Provider } from 'react-redux'
 
 import App from './components/App'
-import store from './slices/index'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>,
+    <Router>
+      <App />
+    </Router>,
     document.getElementById('app')
   )
 })
--- server/server.js
+++ server/server.js
@@ -1,13 +1,10 @@
 const path = require('path')
 const express = require('express')
 
-const playerRoutes = require('./routes/playerRoutes')
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
-
-server.use('/api/v1/players', playerRoutes)
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'index.html'))
