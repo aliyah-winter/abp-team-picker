6dd9979efc1e4296e50d47e95b93d5d2a8dba578
Aliyah Winter <aliyahgwinter@gmail.com>
2023-02-01 17:24:25 +1300

    test server to listen on port env

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-// const server = require('./server')
+const server = require('./server')
 
-// const port = process.env.PORT || 3001
+const port = `0.0.0.0:$PORT`
 
-// server.listen(port, () => {
-//   // eslint-disable-next-line no-console
-//   console.log('Server listening...')
-// })
+server.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server listening...')
+})
