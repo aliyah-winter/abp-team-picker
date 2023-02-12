76c5d19699451ce82283df4f96a6096e2780fe3f
Aliyah Winter <aliyahgwinter@gmail.com>
2023-02-12 21:01:35 +1300

    Update results page

--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -7,13 +7,13 @@ function Results() {
         <ul className="grow mx-9 my-12 p-4 shadow-xl shadow-blue-500 rounded-xl bg-blue-100 text-center">
           <h1 className="text-xl font-heading">WEEK 1</h1>
           <hr className="bg-blue-300 h-0.5 my-4" />
-          <li>BIBLICAL</li>
+          <li className="font-bold">BIBLICAL - 5</li>
           <li>vs</li>
-          <li>MAGIC FINGERS</li>
+          <li>MAGIC FINGERS - 1</li>
           <hr className="bg-blue-300 h-0.5 my-4" />
-          <li>HAMbassadors</li>
+          <li className="font-bold">HAMbassadors - 2</li>
           <li>vs</li>
-          <li>1 OUTZ</li>
+          <li>1 OUTZ - 1</li>
         </ul>
         <ul className="grow mx-9 my-12 p-4 shadow-xl shadow-blue-500 rounded-xl bg-blue-100 text-center">
           <h1 className="text-xl font-heading">WEEK 2</h1>
